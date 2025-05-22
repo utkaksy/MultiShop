f15c90f371f3215210e152d57c464565a72441cb
Utku Aksoy <utkaksy01@hotmail.com>
2025-05-22 22:50:34 +0300

    Ordering API and Service Registiration

---
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Controllers/OrderingsController.cs
@@ -0,0 +1,53 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Order.Application.Features.Mediator.Commands.OrderingCommands;
+using MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
+
+namespace MultiShop.Order.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderingsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderingsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> OrderingList()
+        {
+            var values = await _mediator.Send(new GetOrderingQuery());
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderingById(int id)
+        {
+            var value = await _mediator.Send(new GetOrderingByIdQuery(id));
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrdering(CreateOrderingCommand createOrderingCommand)
+        {
+            await _mediator.Send(createOrderingCommand);
+            return Ok("Sipariş başarıyla eklendi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrdering(UpdateOrderingCommand updateOrderingCommand)
+        {
+            await _mediator.Send(updateOrderingCommand);
+            return Ok("Sipariş Başarıyla Güncellendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteOrdering(int id)
+        {
+            await _mediator.Send(new RemoveOrderingCommand(id));
+            return Ok("Sipariş başarıyla Silindi");
+        }
+    }
+}
