5338d5aac6b6df8067035dc0ebe49b3c2c73a264
Utku Aksoy <utkaksy01@hotmail.com>
2025-05-22 14:51:17 +0300

    OrderDetailHandlers

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/GetOrderDetailByIdQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using MultiShop.Order.Application.Features.CQRS.Queries.AddressQueries;
+using MultiShop.Order.Application.Features.CQRS.Queries.OrderDetailQueires;
+using MultiShop.Order.Application.Features.CQRS.Results.AddressResults;
+using MultiShop.Order.Application.Features.CQRS.Results.OrderDetailResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers
+{
+    public class GetOrderDetailByIdQueryHandler
+    {
+        private readonly IRepository<OrderDetail> _repository;
+        public GetOrderDetailByIdQueryHandler(IRepository<OrderDetail> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetOrderDetailByIdQueryResult> Handle(GetOrderDetailByIdQuery query)
+        {
+            var value = await _repository.GetByIdAsync(query.Id);
+            return new GetOrderDetailByIdQueryResult
+            {
+                OrderDetailId= value.OrderDetailId,
+                ProductAmount = value.ProductAmount,
+                OrderingId = value.OrderingId,
+                ProductId = value.ProductId,
+                ProductName = value.ProductName,
+                ProductPrice = value.ProductPrice,
+                ProductTotalPrice = value.ProductPrice
+            };
+        }
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/UpdateOrderDetailCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers
+{
+    public class UpdateOrderDetailCommandHandler
+    {
+        private readonly IRepository<OrderDetail> _repository;
+        public UpdateOrderDetailCommandHandler(IRepository<OrderDetail> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateOrderDetailCommand updateOrderDetailCommand)
+        {
+            var values = await _repository.GetByIdAsync(updateOrderDetailCommand.OrderDetailId);
+            //values.OrderDetailId = updateOrderDetailCommand.OrderDetailId; parametreden geliyor
+            values.OrderingId = updateOrderDetailCommand.OrderingId;
+            values.ProductId = updateOrderDetailCommand.ProductId;
+            values.ProductPrice = updateOrderDetailCommand.ProductPrice;
+            values.ProductName = updateOrderDetailCommand.ProductName;
+            values.ProductAmount = updateOrderDetailCommand.ProductAmount;
+            values.ProductTotalPrice = updateOrderDetailCommand.ProductTotalPrice;
+            await _repository.UpdateAsync(values);
+        }
+    }
+}
