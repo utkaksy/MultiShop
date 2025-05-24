65a2f58faa787d26d3534db63efc3b89206a02a4
Utku Aksoy <utkaksy01@hotmail.com>
2025-05-24 03:17:13 +0300

    Identity migration

--- IdentityServer/MultiShop.IdentityServer/Models/ApplicationUser.cs
+++ IdentityServer/MultiShop.IdentityServer/Models/ApplicationUser.cs
@@ -9,4 +9,6 @@ namespace MultiShop.IdentityServer.Models;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    public string Name { get; set; }
+    public string  Surname { get; set; }
 }
