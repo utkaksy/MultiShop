1fa0bbc0fa96b14fc8acc0618334c40b8d1e54de
Utku Aksoy <utkaksy01@hotmail.com>
2025-06-03 18:18:30 +0300

    Shopping Cart Page and Contact Page

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ShoppingCartViewComponent/_ShoppingCartDiscountCouponComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ShoppingCartViewComponent
+{
+    public class _ShoppingCartDiscountCouponComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
