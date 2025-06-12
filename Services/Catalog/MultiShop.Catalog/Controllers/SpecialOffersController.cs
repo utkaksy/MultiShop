3420aaaab6844263028f224de23070fda591cfb5
Utku Aksoy <utkaksy01@hotmail.com>
2025-06-12 13:07:10 +0300

    Dynamic Feature Slider And Special Offer

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/SpecialOffersController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.SpecialOfferDtos;
+using MultiShop.Catalog.Services.CategoryServices;
+using MultiShop.Catalog.Services.SpecialOfferServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    //[Authorize]
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpecialOffersController : ControllerBase
+    {
+        private readonly ISpecialOfferService _specialOfferService;
+        public SpecialOffersController(ISpecialOfferService specialOfferService)
+        {
+            _specialOfferService = specialOfferService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SpecialOfferList()
+        {
+            var values = await _specialOfferService.GetAllSpecialOfferAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSpecialOfferById(string id)
+        {
+            var value = await _specialOfferService.GetByIdSpecialOfferAsync(id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSpecialOffer(CreateSpecialOfferDto createSpecialOfferDto)
+        {
+            await _specialOfferService.CreateSpecialOfferAsync(createSpecialOfferDto);
+            return Ok("Özel Teklif Başarıyla Eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteSpecialOffer(string id)
+        {
+            await _specialOfferService.DeleteSpecialOfferAsync(id);
+            return Ok("Özel Teklif Başarıyla Silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateSpecialOffer(UpdateSpecialOfferDto updateSpecialOfferDto)
+        {
+            await _specialOfferService.UpdateSpecialOfferAsync(updateSpecialOfferDto);
+            return Ok("Özel Teklif Başarıyla Güncellendi");
+        }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Services/SpecialOfferServices/ISpecialOfferService.cs
@@ -0,0 +1,14 @@
+﻿using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Dtos.SpecialOfferDtos;
+
+namespace MultiShop.Catalog.Services.SpecialOfferServices
+{
+    public interface ISpecialOfferService
+    {
+        Task<List<ResultSpecialOfferDto>> GetAllSpecialOfferAsync();
+        Task<GetByIdSpecialOfferDto> GetByIdSpecialOfferAsync(string id);
+        Task DeleteSpecialOfferAsync(string id);
+        Task CreateSpecialOfferAsync(CreateSpecialOfferDto createSpecialOfferDto);
+        Task UpdateSpecialOfferAsync(UpdateSpecialOfferDto updateSpecialOfferDto);
+    }
+}
