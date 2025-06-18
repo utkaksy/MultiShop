fc508a97931d9134f6fa0f4f2533e6a9c4cca7fe
Utku Aksoy <utkaksy01@hotmail.com>
2025-06-18 16:31:36 +0300

    Comment

---
+++ Services/Comment/MultiShop.Comment/Controllers/CommentsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Comment.Context;
+using MultiShop.Comment.Entites;
+using System.Threading.Tasks;
+
+namespace MultiShop.Comment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class CommentsController : ControllerBase
+    {
+        private readonly CommentContext _commentContext;
+
+        public CommentsController(CommentContext commentContext)
+        {
+            _commentContext = commentContext;
+        }
+
+        [HttpGet]
+        public IActionResult CommentList()
+        {
+            var values = _commentContext.UserComments.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateComment(UserComment userComment)
+        {
+            _commentContext.UserComments.Add(userComment);
+            _commentContext.SaveChanges();
+            return Ok("Yorum Başarıyla Eklendi");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteComment(int id)
+        {
+            var value = _commentContext.UserComments.Find(id);
+            _commentContext.UserComments.Remove(value);
+            _commentContext.SaveChanges();
+            return Ok("Yorum Başarıyla Silindi");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetByIdComment(int id)
+        {
+
+            var value = _commentContext.UserComments.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateComment(UserComment userComment)
+        {
+            _commentContext.UserComments.Update(userComment);
+            _commentContext.SaveChanges();
+            return Ok("Yorum Başarıyla Güncellendi");
+        }
+
+        [HttpGet("CommentListByProductId")]
+        public IActionResult CommentListByProductId(string id)
+        {
+            var values = _commentContext.UserComments.Where(x=>x.ProductId==id).ToList();
+            return Ok(values);
+        }
+    }
+}
